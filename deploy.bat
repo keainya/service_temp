a0eb68ceceec57d29f43fd8ede41b9b4faa886d3
keainya <xieboqian@ustc.edu>
2026-05-31 18:09:47 +0800

    go mod tidy

--- deploy.bat
+++ deploy.bat
@@ -6,5 +6,5 @@ ssh root@takemeto.icu "mkdir /apps/service_temp 2>/dev/null"
 ssh root@takemeto.icu "rm -f /apps/service_temp/app.out 2>/dev/null"
 scp .\app.out root@takemeto.icu:/apps/service_temp/app.out
 ssh root@takemeto.icu "chmod +x /apps/service_temp/app.out"
-ssh root@takemeto.icu "kill $(ps aux | grep /apps/service_temp/app.out | grep -v grep | awk '{print $2}')"
+ssh root@takemeto.icu "kill $(ps aux | grep /apps/service_temp/app.out | grep -v grep | awk '{print $2}') 2>/dev/null"
 ssh root@takemeto.icu "cd /apps/service_temp && nohup /apps/service_temp/app.out > /dev/null 2>&1 &"
