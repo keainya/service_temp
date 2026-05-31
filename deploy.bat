91598a186a8de0080d6598aa3dd80d9c8ec9cde6
keainya <xieboqian@ustc.edu>
2026-05-31 18:04:19 +0800

    部署模板

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+set GOOS=linux
+go build -o app.out
+set GOOS=windows
+ssh root@takemeto.icu "rm -f /apps/service_temp/app.out"
+scp .\app.out root@takemeto.icu:/apps/service_temp/app.out
+ssh root@takemeto.icu "chmod +x /apps/service_temp/app.out"
+ssh root@takemeto.icu "kill $(ps aux | grep /apps/service_temp/app.out | grep -v grep | awk '{print $2}')"
+ssh root@takemeto.icu "cd /apps/service_temp && nohup /apps/service_temp/app.out > /dev/null 2>&1 &"
