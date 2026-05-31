c696c5d78ee812457e8de675ed753b4ae4c3e40e
keainya <xieboqian@ustc.edu>
2026-05-31 18:07:15 +0800

    创建App环境

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,9 @@
 set GOOS=linux
 go build -o app.out
 set GOOS=windows
-ssh root@takemeto.icu "rm -f /apps/service_temp/app.out"
+ssh root@takemeto.icu "mkdir /apps 2>/dev/null"
+ssh root@takemeto.icu "mkdir /apps/service_temp 2>/dev/null"
+ssh root@takemeto.icu "rm -f /apps/service_temp/app.out 2>/dev/null"
 scp .\app.out root@takemeto.icu:/apps/service_temp/app.out
 ssh root@takemeto.icu "chmod +x /apps/service_temp/app.out"
 ssh root@takemeto.icu "kill $(ps aux | grep /apps/service_temp/app.out | grep -v grep | awk '{print $2}')"
